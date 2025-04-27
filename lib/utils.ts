fbbcfe64c2cf37c2d7b630ed211962cfe6deaaea
jinx6ix <irayaian15@gmail.com>
2025-04-27 13:28:57 +0300

    utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
+
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-=======
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
->>>>>>> 44171c7 (new)
